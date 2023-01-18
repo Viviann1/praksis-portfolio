83c46f7be5a3a51f3f36d635fe1eddee1b254445
Viviann1 <viviann.kodehode@gmail.com>
2023-01-18 16:29:06 +0100

    adding assets folder mm

---
+++ src/components/header/CTS.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href=''>Last ned CV</a>
+        <a href='#contact'>La oss snakke litt</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,14 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hei, jeg heter</h5>
+        <h1>Viviann Heggestad</h1>
+        <h5 className='text-light'>Designer and frontend utvikler</h5>
+
+      </div>
+    </header>
   )
 }
 
