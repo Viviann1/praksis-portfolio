46d079bbd1a54d2aeb68c845edce426b98b8677b
Viviann1 <viviann.kodehode@gmail.com>
2023-01-24 10:40:34 +0100

    Addes HeaderSocials component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/modifiedme.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,12 +12,14 @@ const Header = () => {
         <h1>Viviann Heggestad</h1>
         <h5 className='text-light'>Designer and frontend utvikler</h5>
         <CTA />
+        <HeaderSocials />
         <div className='me'>
           <img src={ME} alt='me'/>
         </div>
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
 }
-
+ 
 export default Header
