6e4957118ecefb0eee545c3b9078bb1ae5728273
Viviann1 <viviann.kodehode@gmail.com>
2023-01-25 22:14:55 +0100

    testimonials some css

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,18 @@ import './testimonials.css'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Mine</h5>
+      <h2>referanser</h2>
+
+      <div className="container.testimonials__container">
+
+        <article className="testimonials">
+         <h5>Oppgis ved forespørsel</h5> 
+           </article>
+
+      </div>
+    </section>
   )
 }
 
