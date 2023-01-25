be968c271283ff9edd31f34117fcc442b8283318
Viviann1 <viviann.kodehode@gmail.com>
2023-01-25 20:51:10 +0100

    portfolio component og css

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/moviestream.jpg'
+import IMG2 from '../../assets/countdown.jpg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>Min design</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='' />
+          </div>
+          <h3>Movie streamer</h3>
+          <div className='portfolio__item-cta'> 
+          <a href="https://github.com" className='btn'>Github</a>
+          <a href="https://www.figma.com/file/S8o5n1TUES4EfF4fmdwugc/Modul-C-sep22---Viviann-Moviestreamer?node-id=0%3A1&t=VatVqMMaGNZSWKml-1" className='btn btn-primary' target='_blank'>Figma</a>
+          </div>
+        </article>
+        
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt='' />
+          </div>
+          <h3>Nedteller til jul</h3>
+          <div className="portfolio__items-cta"> 
+          <a href="https://github.com" className='btn'>Github</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
