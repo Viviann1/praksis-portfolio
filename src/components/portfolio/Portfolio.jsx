dfedd654640331cb097bd41c69e3a94ba4044988
Viviann1 <107039059+Viviann1@users.noreply.github.com>
2023-05-05 11:15:49 +0200

    Portfolio adjusted styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/moviestream.jpg'
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      <h5>Min design</h5>
-      <h2>Portfolio</h2>
+      <h5 className='h5_heading'>Min design</h5>
+      <h2 className='h2_heading'>Portfolio</h2>
 
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
-          <h3>Movie streamer</h3>
+          <h3 className='h3_heading'>Movie streamer</h3>
           <div className='portfolio__item-cta'> 
           <a href="https://www.figma.com/file/S8o5n1TUES4EfF4fmdwugc/Modul-C-sep22---Viviann-Moviestreamer?node-id=0%3A1&t=VatVqMMaGNZSWKml-1" className='btn btn-primary' rel='_external'>Figma</a>
           </div>
         </article>
         
         <article className='portfolio__item'>
-          <h3>Nedteller til jul</h3>
-          <div className="portfolio__items-cta"> 
+          <h3 className='h3_heading'>Nedteller til jul</h3>
+          <div className="portfolio__item-cta"> 
           <a href="https://github.com/Viviann1/juleoppgave.git" className='btn'>Github</a>
           </div>
         </article>
+
+        <article className='portfolio__item'>
+          <h3 className='h3_heading'>Hjerteting</h3>
+          <div className="portfolio__item-cta"> 
+          <a href="https://www.hjerteting.no/" className='btn'>Nettbutikk</a>
+          </div>
+        </article>
+
       </div>
     </section>
   )
