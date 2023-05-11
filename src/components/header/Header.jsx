f81d176a33099c70f1512de6698cb19e399adcc6
Viviann1 <107039059+Viviann1@users.noreply.github.com>
2023-05-11 12:48:53 +0200

    changed header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/paint.jpg'
 import ME2 from '../../assets/me.jpg'
+import ME3 from '../../assets/me3.jpg'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -11,7 +12,7 @@ const Header = () => {
       <div className="container header__container">
         <h5 className='text-light'>Hei!</h5>
         <h1>Viviann Heggestad</h1>
-        <h5 className='text-light'>Designer and frontend utvikler</h5>
+        <h5 className='text-light'>Frontend utvikler, designer og blomster elsker</h5>
         <CTA />
         <HeaderSocials />
         <div className='me'>
@@ -19,6 +20,9 @@ const Header = () => {
         </div>
         <div className='me2'>
           <img src={ME2} alt='me2' />
+        </div>
+        <div className='me3'>
+          <img src={ME3} alt='me3' />
         </div>
         <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
