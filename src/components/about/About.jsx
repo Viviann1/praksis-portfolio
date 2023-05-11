281bccf6bae11db05221a6794c3279d33c71dc1d
Viviann1 <107039059+Viviann1@users.noreply.github.com>
2023-05-11 14:49:56 +0200

    text changes in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/aboutme.jpg'
+import ABOUT2 from '../../assets/aboutme.jpg'
 import {FiAward} from 'react-icons/fi'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderOpened} from 'react-icons/vsc'
@@ -14,7 +14,7 @@ const About = () => {
       <div className='container about__container'>
           <div className="about__me">
             <div className="about__me-image">
-              <img src={ME} alt='About Image' />
+              <img src={ABOUT2} alt='About Image' />
             </div>
           </div>
           
@@ -23,8 +23,9 @@ const About = () => {
             <article className='about__card'>
               <FiAward className='about__icon' />
               <h5>Erfaring</h5>
-              <p>7+ års erfaring som art director og grafisk designer.</p>
               <p>Frontend Utvikler hovedvekt på design.</p>
+              <p>7+ års erfaring som Art Director og Grafisk Designer.</p>
+              <p>Content manager for Hjerteting.</p>
             </article>
 
             <article className='about__card'>
@@ -32,26 +33,24 @@ const About = () => {
               <h5>Kunder</h5>
               <p>Jobbet med både små og store bedrifter.</p>
               <p>Vant med å jobbe direkte med kunden.</p>
+              <p>Tidligere kunder Telenor, Procean, Radisson Hotel Bergen, BBT, Kärcher mm.</p>
             </article>
 
             <article className='about__card'>
               <VscFolderOpened className='about__icon' />
               <h5>Prosjekter</h5>
-              <h6>Nylært frontend utvikler</h6>
-              <p>noe kode og noe design</p>
+              <p>Nettbutikkløsning <br />Hjerteting</p>
+              <p>Jobbjakt nettside - jobbet i team på løsning</p>
+              <p>Art Director Telenorbygget Kokstad brosjyrer ved åpning</p>
             </article>
 
           </div>
-          <p>Kreativ sjel med bakgrunn som grafisk designer/
-AD i reklamebyråer.
-Jobbet som grafisk designer i Gazette 
-- gratis-magasin på Lanzarote. 
-Var med på å starte opp og drev kunst/håndverksbutikk/kollektiv i Irland.  
-Vil nå tilbake til å jobbe mer med det kreative 
-og vil derfor utvide mine kunnskaper. 
-Jobber nå hardt for å bli god på koding og kunne designe mine egne nettsider. 
-Har mange idèer 
-- og ser fram til å få de gjennomført!</p>
+          <p>Kreativ sjel som for tiden er i praksis som Frontend Utvikler i Kodeverket Bergen.<br />  
+            Min styrke ligger i designerfaring som gjenspeiles i min kode.
+      Har bakgrunn som Art Director/Grafisk Designer i reklamebyråer. 
+      Jobbet som Grafisk Designer i Gazette, et gratis-magasin på Lanzarote. 
+      Var med på å starte opp og drev kunst og håndverksbutikk i Irland.  
+</p>
 
 <a href='#contact' className='btn btn-primary'>Ta kontakt</a>
           </div>
