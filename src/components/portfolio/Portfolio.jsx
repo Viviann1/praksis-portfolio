ec0065e5f2117917e9ccd702da25e774af45ce41
Viviann1 <107039059+Viviann1@users.noreply.github.com>
2023-08-07 16:26:56 +0200

    Added files to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,21 +9,30 @@ const Portfolio = () => {
 
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
+          <div>
           <h3 className='h3_heading'>Movie streamer</h3>
+          <p className='portfolio-p'>Skoleoppgave Kodehode</p>
+          </div>
           <div className='portfolio__item-cta'> 
           <a href="https://www.figma.com/file/S8o5n1TUES4EfF4fmdwugc/Modul-C-sep22---Viviann-Moviestreamer?node-id=0%3A1&t=VatVqMMaGNZSWKml-1" className='btn btn-primary' rel='_external'>Figma</a>
           </div>
         </article>
         
         <article className='portfolio__item'>
+          <div>
           <h3 className='h3_heading'>Nedteller jul</h3>
+          <p className='portfolio-p'>Skoleoppgave Kodehode</p>
+          </div>
           <div className="portfolio__item-cta"> 
           <a href="https://github.com/Viviann1/juleoppgave.git" className='btn'>Github</a>
           </div>
         </article>
 
         <article className='portfolio__item'>
+          <div>
           <h3 className='h3_heading'>Hjerteting</h3>
+          <p className='portfolio-p'>Butikkløsning team Kodeverket</p>
+          </div>
           <div className="portfolio__item-cta"> 
           <a href="https://www.hjerteting.no/" className='btn'>Nettbutikk</a>
           </div>
@@ -36,6 +45,16 @@ const Portfolio = () => {
           </div>
           <div className="portfolio__item-cta"> 
           <a href="https://www.jobbjakt.no/" className='btn'>Nettside</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div>
+          <h3 className='h3_heading'>Jobbjakt design</h3>
+          <p className='portfolio-p'>Min designskisse</p>
+          </div>
+          <div className="portfolio__item-cta"> 
+          <a href="https://www.figma.com/file/7eIMFyCfICCeIndThPwvdK/Jobbjakt-forslag?type=design&node-id=0%3A1&mode=design&t=r6tFCsiU3mP2j3gG-1" className='btn'>Figma</a>
           </div>
         </article>
 
